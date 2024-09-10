17b5c2ecad765abbf3012ac671dfd90cdcf4656b
Etativel <farhanmaulana4611@gmail.com>
2024-09-10 21:32:12 +0700

    add slideshow functionality

--- README.md
+++ README.md
@@ -13,3 +13,15 @@ yarn build
 ```
 
 to bundle your application
+
+Live preview :
+
+Resources used:
+
+- https://www.freepik.com/free-photo/rock-ocean-landscape_1266637.htm#query=scenery&position=0&from_view=keyword&track=ais_hybrid&uuid=6e36cab0-f04b-44ee-81ed-6b7e54743dfb
+
+- https://www.freepik.com/free-photo/landscape-morning-fog-mountains-sunrise_13180511.htm#query=scenery&position=1&from_view=keyword&track=ais_hybrid&uuid=6e36cab0-f04b-44ee-81ed-6b7e54743dfb
+
+- https://www.freepik.com/free-ai-image/hawaii-beach-landscape-with-ocean-view_299834426.htm#query=scenery&position=2&from_view=keyword&track=ais_hybrid&uuid=6e36cab0-f04b-44ee-81ed-6b7e54743dfb
+
+- https://www.freepik.com/free-photo/sunrise-morning-mist-phu-lang-ka-phayao-thailand_13180839.htm#query=scenery&position=7&from_view=keyword&track=ais_hybrid&uuid=be4eea13-4b50-47dc-96c6-cb8eb3e1b9b4
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import "./styles/style.css";
+
+// Dropdown menu
 const closeBtn = document.querySelectorAll(".close-dropdown");
 closeBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
     const buttons = document.querySelectorAll(".nav-btn");
     buttons.forEach((button) => {
       button.style.backgroundColor = "black";
-      button.style.color = "white";
+      button.style.color = "pink";
     });
     btn.closest(".dropdown-content").style.visibility = "hidden";
   });
@@ -29,11 +31,85 @@ buttons.forEach((button) => {
   button.addEventListener("click", () => {
     buttons.forEach((button) => {
       button.style.backgroundColor = "black";
-      button.style.color = "white";
+      button.style.color = "pink";
     });
-    button.style.backgroundColor = "white";
+    button.style.backgroundColor = "pink";
     button.style.color = "black";
     console.log(button.classList[0].split("-")[0]);
     showDropdown(button.classList[0].split("-")[0]);
   });
+});
+
+// Image slideshow
+import image1 from "./images/hawaiiBeach.jpg";
+import image2 from "./images/morningFog.jpg";
+import image3 from "./images/rockOcean.jpg";
+import image4 from "./images/sunriseMorning.jpg";
+const img1 = document.querySelector(".image-1");
+const img2 = document.querySelector(".image-2");
+const img3 = document.querySelector(".image-3");
+const img4 = document.querySelector(".image-4");
+
+img1.src = image1;
+img2.src = image2;
+img3.src = image3;
+img4.src = image4;
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+prevBtn.addEventListener("click", () => {
+  plusSlides(-1);
+});
+nextBtn.addEventListener("click", () => {
+  plusSlides(1);
+});
+
+const dot1 = document.querySelector(".d1");
+const dot2 = document.querySelector(".d2");
+const dot3 = document.querySelector(".d3");
+const dot4 = document.querySelector(".d4");
+
+dot1.addEventListener("click", () => {
+  currentSlide(1);
+});
+dot2.addEventListener("click", () => {
+  currentSlide(2);
+});
+dot3.addEventListener("click", () => {
+  currentSlide(3);
+});
+dot4.addEventListener("click", () => {
+  currentSlide(4);
 });
