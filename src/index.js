8c98aada1dcfde829211a1d00142e2cdec93a9fa
Etativel <farhanmaulana4611@gmail.com>
2024-09-10 16:14:57 +0700

    dropdown functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import "./styles/style.css";
-const dom = document.querySelector("body");
-dom.innerHTML = "hello world";
+const closeBtn = document.querySelectorAll(".close-dropdown");
+closeBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.closest(".dropdown-content").style.visibility = "hidden";
+  });
+});
+
+function showDropdown(content) {
+  const allDropdown = document.querySelectorAll(".dropdown-content");
+  allDropdown.forEach((btn) => {
+    btn.style.visibility = "hidden";
+  });
+  const dropdownDOM = document.querySelector(`.${content}.dropdown-content`);
+  if (dropdownDOM.style.visibility === "visible") {
+    dropdownDOM.style.visibility = "hidden";
+  } else {
+    dropdownDOM.style.visibility = "visible";
+  }
+}
+
+const tutorialBtn = document.querySelector(".tutorial-btn");
+const exercisesBtn = document.querySelector(".exercises-btn");
+const certificatesBtn = document.querySelector(".certificates-btn");
+const servicesBtn = document.querySelector(".services-btn");
+
+tutorialBtn.addEventListener("click", () => {
+  showDropdown("tutorial");
+});
+exercisesBtn.addEventListener("click", () => {
+  showDropdown("exercises");
+});
+certificatesBtn.addEventListener("click", () => {
+  showDropdown("certificates");
+});
+servicesBtn.addEventListener("click", () => {
+  showDropdown("services");
+});
